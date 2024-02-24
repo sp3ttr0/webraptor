01d847f05c18b9bbe64e473efd5ecaf6ab7d625a
sp3ttr0 <77888892+sp3ttr0@users.noreply.github.com>
2024-02-24 20:01:06 +0800

    Update bb.py

--- bb.py
+++ bb.py
@@ -141,7 +141,7 @@ def run_nuclei():
 
     print(f"{Fore.BLUE}[*] Running nuclei against live subdomains...{Style.RESET_ALL}")
     # Define the subprocess command
-    nuclei_command = ["nuclei", "-l", f"{domain}/subs_live.txt", "-etags", "ssl,dns,header,security", "-silent", "-o", "{domain}/nuclei.txt"]
+    nuclei_command = ["nuclei", "-l", f"{domain}/subs_live.txt", "-etags", "ssl,dns,headers", "-silent", "-o", f"{domain}/nuclei.txt"]
     
     # Run the Nuclei process
     nuclei_process = subprocess.Popen(nuclei_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
